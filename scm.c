e88eeba9f439869acf0abe429c18b2a0bb9e20fe
Lucas Sobral <lucasdecarvalhosobral@gmail.com>
2023-08-10 16:32:11 -0300

    add divisao

--- scm.c
+++ scm.c
@@ -8,9 +8,17 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void divisao(int a, int b) {
+    if (b == 0) {
+        printf("Erro: Divisão por zero não é permitida.\n");
+    } else {
+        printf("%d / %d = %d\n", a, b, a / b);
+    }
+}
+
 int main() {
-    printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    divisao(10, 2);
     return 0;
 }
